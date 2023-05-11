e46c633a41644a1834eb123af0ba8c7802b96293
pranavsai14 <sai.margasahayamvenkatesh@stud.fra-uas.de>
2023-05-11 22:44:35 +0200

    Added python implementation of ttl to text file conversion

--- test/test_description_graph.py
+++ test/test_description_graph.py
@@ -1,16 +1,29 @@
+import sys
 from rdflib import Graph, URIRef
+
+sys.path.append('C:/Users/prana/Desktop/Masters Thesis/')
 from description_graph import DescriptionGraph
+
 
 class TestDescriptionGraph:
 
     def test_unravel(self):
         base_URI = "http://example.org/"
-        a = URIRef(base_URI + "a")
-        b = URIRef(base_URI + "b")
-        c = URIRef(base_URI + "c")
+        x1 = URIRef(base_URI + "x1")
+        y1 = URIRef(base_URI + "y1")
+        z1 = URIRef(base_URI + "z1")
 
-        dg = DescriptionGraph("test_files/unravel-test-0.ttl")
-        depth = 1
-        dt = dg.unravel(a, depth)
-        print(dt.to_str())
-        assert True
+        x2 = URIRef(base_URI + "x2")
+        y2 = URIRef(base_URI + "y2")
+        z2 = URIRef(base_URI + "z2")
+
+        dg = DescriptionGraph("C:/Users/prana/Desktop/Masters Thesis/test_files/example-product-1.ttl")
+        depth = 2
+        dt = dg.unravel(x1, depth)
+        dt = dt.to_str()
+        return dt
+
+
+result = TestDescriptionGraph()
+print(result.test_unravel())
+assert True
